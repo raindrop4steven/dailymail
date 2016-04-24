4e09b687ae6b7a1c4ca9fbef7c39f31ee8cc5283
rockboyfor <rockboyfor@gmail.com>
2016-04-24 22:36:27 +0800

    move common header into config.ini

--- login.py
+++ login.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import ConfigParser
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_formhash():
     """ Get formhash from editorForm"""
-    # Set request form header info
-    request_form_header = {
-        'Host': 'translate.chinadaily.com.cn',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate',
-        'Referer': 'http://translate.chinadaily.com.cn/portal.php?mod=portalcp&ac=article',
-        'Cookie': 'q8vf_2132_saltkey=KM66kb68; q8vf_2132_lastvisit=1461328499; q8vf_2132_lastact=1461368884%09api.php%09js; pgv_pvi=8652438736; y50Z_2132_sid=Y0ZqO3; y50Z_2132_saltkey=RKJKzlKC; y50Z_2132_lastvisit=1461340300; y50Z_2132_lastact=1461370676%09home.php%09editor; y50Z_2132_ulastactivity=9ed5RRxYlU46SB6r%2Fx%2BfHJ4utl3rFSX2fmP%2FpVIRaV2KdUbWtBQg; y50Z_2132_auth=0b93paD3LXW4VVHpolwb8l2VW%2Fe9y04IaPq6fBly9dVCSEFTM4vRzbLPbZijpKI2YJZXJavStNk3qChQqKrjWsJDjSZe; y50Z_2132_lastcheckfeed=1603783%7C1461343917; y50Z_2132_security_cookiereport=5669fYPwe%2FOau0wELTn%2BCGZpw7IaPYJu4gRuGyE%2B8V9CZd1QfcGc; pgv_info=ssi=s869159533; y50Z_2132_sendmail=1; y50Z_2132_checkpm=1; tjpctrl=1461372601633',
-        'DNT': '1',
-        'Connection': 'keep-alive'
-    }
+
+    # Get common headers first
+    request_form_header = get_common_header()
+
+    # Add special header for GET
+    request_form_header['DNT'] = '1'
 
     # Get formhash from response
     response = requests.get('http://translate.chinadaily.com.cn/portal.php?mod=portalcp&ac=article',
@@ -34,17 +29,8 @@ def get_formhash():
 def post_article(form_hash, boundary, fromurl, title, summary, content):
     """ Post article """
     # Construct request_form_header
-    post_form_header = {
-        'Host': 'translate.chinadaily.com.cn',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate',
-        'Referer': 'http://translate.chinadaily.com.cn/portal.php?mod=portalcp&ac=article',
-        'Cookie': 'q8vf_2132_saltkey=KM66kb68; q8vf_2132_lastvisit=1461328499; q8vf_2132_lastact=1461368884%09api.php%09js; pgv_pvi=8652438736; y50Z_2132_sid=Y0ZqO3; y50Z_2132_saltkey=RKJKzlKC; y50Z_2132_lastvisit=1461340300; y50Z_2132_lastact=1461370676%09home.php%09editor; y50Z_2132_ulastactivity=9ed5RRxYlU46SB6r%2Fx%2BfHJ4utl3rFSX2fmP%2FpVIRaV2KdUbWtBQg; y50Z_2132_auth=0b93paD3LXW4VVHpolwb8l2VW%2Fe9y04IaPq6fBly9dVCSEFTM4vRzbLPbZijpKI2YJZXJavStNk3qChQqKrjWsJDjSZe; y50Z_2132_lastcheckfeed=1603783%7C1461343917; y50Z_2132_security_cookiereport=5669fYPwe%2FOau0wELTn%2BCGZpw7IaPYJu4gRuGyE%2B8V9CZd1QfcGc; pgv_info=ssi=s869159533; y50Z_2132_sendmail=1; y50Z_2132_checkpm=1; tjpctrl=1461372601633',
-        'Connection': 'keep-alive',
-        'Content-Type': 'multipart/form-data; boundary=%s' % boundary
-    }
+    post_form_header = get_common_header()
+    post_form_header['Content-Type'] = ('multipart/form-data; boundary=%s' % boundary)
 
     # Post data
     post_data = {
@@ -92,6 +78,14 @@ def add_article(fromurl, title, summary, content):
 
     return response
 
+
+def get_common_header():
+    """ Get common header from config.ini """
+    cf = ConfigParser.ConfigParser()
+    cf.read('config.ini')
+    headers = cf.items('headers')
+
+    return dict(headers)
 
 if __name__ == '__main__':
     # Set boundary
